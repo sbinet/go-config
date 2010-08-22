c6b701618e4cd5e104934d5e970086754e91002d
John Mac <charles.beck@sent.com>
2010-08-22 11:40:26 +0100

    Structure generated by gowizard.
    --HG--
    extra : convert_revision : edccf46e5fa1c37faf847c52cc8f0f6b1359649d

---
+++ config/main.go
@@ -0,0 +1,20 @@
+// Copyright 2009  The "goconfig" Authors
+//
+// Use of this source code is governed by the Simplified BSD License
+// that can be found in the LICENSE file.
+//
+// This software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, either express or implied. See the License
+// for more details.
+
+package config
+
+import (
+
+)
+
+
+func () {
+
+}
+
